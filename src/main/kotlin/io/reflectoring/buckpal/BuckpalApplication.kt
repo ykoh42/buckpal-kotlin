ea444d37ab491454bd390fa7fe28572330d8ad08
YOUNGHWAN KOH <ykoh42@ykohs-MacBook-Pro.local>
2023-07-01 22:49:27 +0900

    refactor: application name

--- src/main/kotlin/io/reflectoring/buckpal/BuckpalApplication.kt
+++ src/main/kotlin/io/reflectoring/buckpal/BuckpalApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BuckpalKotlinApplication
+class BuckpalApplication
 
 fun main(args: Array<String>) {
-	runApplication<BuckpalKotlinApplication>(*args)
+	runApplication<BuckpalApplication>(*args)
 }
