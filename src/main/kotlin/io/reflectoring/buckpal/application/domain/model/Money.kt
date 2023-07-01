d13239ea3dfaf642e3ba85381daa3f64b25ee0a0
YOUNGHWAN KOH <ykoh42@ykohs-MacBook-Pro.local>
2023-07-01 22:44:18 +0900

    style: format

--- src/main/kotlin/io/reflectoring/buckpal/application/domain/model/Money.kt
+++ src/main/kotlin/io/reflectoring/buckpal/application/domain/model/Money.kt
@@ -3,9 +3,7 @@ package io.reflectoring.buckpal.application.domain.model
 import java.math.BigInteger
 
 @JvmInline
-value class Money(
-    private val amount: BigInteger,
-) {
+value class Money(private val amount: BigInteger) {
     operator fun plus(other: Money): Money = Money(amount = this.amount + other.amount)
 
     operator fun minus(other: Money): Money = Money(amount = this.amount - other.amount)
